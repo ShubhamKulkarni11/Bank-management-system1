8c04c8aab4907db9e1ee94113aaff9ca3520c9e0
ShubhamKulkarni11 <kulkarnishubham228@gmail.com>
2021-04-15 09:53:55 +0530

    commit 3

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '01&@5y9(6tq$!jkqus_&*_+!vvbyo#5%m9_=(8(41g8b!2^1z3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- bank_management/urls.py
+++ bank_management/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include,path
+from tornado.web import url
+from django.views.static import serve
+from django.conf.urls import url
 from . import views
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('bank/',include('bank.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
